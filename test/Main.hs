e0650a9c7d1b85b8ebe4695cba7b862d500ac4b6
Bodigrim <andrew.lelechenko@gmail.com>
2019-04-21 23:05:55 +0100

    Test subtraction against a reference

--- test/Main.hs
+++ test/Main.hs
@@ -15,7 +15,7 @@ import Test.QuickCheck.Classes
 
 main :: IO ()
 main = defaultMain $ testGroup "All"
-    [ additionTests
+    [ arithmeticTests
     , semiringTests
     ]
 
@@ -36,12 +36,22 @@ instance (Eq a, Semiring a, Arbitrary a) => Arbitrary (Poly a) where
   arbitrary = toPoly' . V.fromList <$> arbitrary
   shrink = fmap (toPoly' . V.fromList) . shrink . V.toList . unPoly
 
-additionTests :: TestTree
-additionTests = testProperty "addition matches reference" $
-  \(xs :: [Int]) ys -> toPoly (V.fromList (addRef xs ys)) ===
-    toPoly (V.fromList xs) + toPoly (V.fromList ys)
+arithmeticTests :: TestTree
+arithmeticTests = testGroup "Arithmetic"
+  [ testProperty "addition matches reference" $
+    \(xs :: [Int]) ys -> toPoly (V.fromList (addRef xs ys)) ===
+      toPoly (V.fromList xs) + toPoly (V.fromList ys)
+  , testProperty "subtraction matches reference" $
+    \(xs :: [Int]) ys -> toPoly (V.fromList (subRef xs ys)) ===
+      toPoly (V.fromList xs) - toPoly (V.fromList ys)
+  ]
 
 addRef :: Num a => [a] -> [a] -> [a]
 addRef [] ys = ys
 addRef xs [] = xs
 addRef (x : xs) (y : ys) = (x + y) : addRef xs ys
+
+subRef :: Num a => [a] -> [a] -> [a]
+subRef [] ys = map negate ys
+subRef xs [] = xs
+subRef (x : xs) (y : ys) = (x - y) : subRef xs ys
