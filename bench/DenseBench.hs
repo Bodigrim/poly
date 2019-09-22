d05fe959c97fe641adc248597b1c2e7bc7052706
Bodigrim <andrew.lelechenko@gmail.com>
2019-09-22 23:27:37 +0100

    #22 More benchmarks for gcdExt

--- bench/DenseBench.hs
+++ bench/DenseBench.hs
@@ -1,16 +1,24 @@
-{-# LANGUAGE CPP        #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE CPP                        #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE TypeApplications           #-}
 
 module DenseBench
   ( benchSuite
   ) where
 
 import Prelude hiding (quotRem, gcd)
+import Control.DeepSeq
 import Gauge.Main
 import Data.Poly
 import qualified Data.Vector.Unboxed as U
 #if MIN_VERSION_semirings(0,4,2)
+import Control.Exception
+import Data.Bits
+import Data.Coerce
 import Data.Euclidean
+import Data.Semiring (Semiring(..), Ring, isZero)
+import qualified Data.Semiring as S (negate)
 import qualified Data.Vector as V
 #endif
 
@@ -22,10 +30,12 @@ benchSuite = bgroup "dense" $ concat
   , map benchDeriv    [100, 1000, 10000]
   , map benchIntegral [100, 1000, 10000]
 #if MIN_VERSION_semirings(0,4,2)
-  , map benchQuotRem  [10, 100]
-  , map benchGcd      [10, 100]
-  , map benchGcdExt   [10, 100]
-  , map benchGcdFrac  [10, 100]
+  , map benchQuotRem    [10, 100]
+  , map benchGcd        [10, 100]
+  , map benchGcdExtRat  [10, 20, 40]
+  , map benchGcdFracRat [10, 20, 40]
+  , map benchGcdExtM    [10, 100, 1000]
+  , map benchGcdFracM   [10, 100, 1000]
 #endif
   ]
 
@@ -52,11 +62,17 @@ benchQuotRem k = bench ("quotRem/" ++ show k) $ nf doQuotRem k
 benchGcd :: Int -> Benchmark
 benchGcd k = bench ("gcd/" ++ show k) $ nf doGcd k
 
-benchGcdExt :: Int -> Benchmark
-benchGcdExt k = bench ("gcdExt/" ++ show k) $ nf doGcdExt k
+benchGcdExtRat :: Int -> Benchmark
+benchGcdExtRat k = bench ("gcdExt/Rational/" ++ show k) $ nf (doGcdExt @Rational) k
 
-benchGcdFrac :: Int -> Benchmark
-benchGcdFrac k = bench ("gcdFrac/" ++ show k) $ nf doGcdFrac k
+benchGcdFracRat :: Int -> Benchmark
+benchGcdFracRat k = bench ("gcdFrac/Rational/" ++ show k) $ nf (doGcdFrac @Rational) k
+
+benchGcdExtM :: Int -> Benchmark
+benchGcdExtM k = bench ("gcdExt/Mod2/" ++ show k) $ nf (doGcdExt @Mod2) k
+
+benchGcdFracM :: Int -> Benchmark
+benchGcdFracM k = bench ("gcdFrac/Mod2/" ++ show k) $ nf (doGcdFrac @Mod2) k
 
 #endif
 
@@ -87,32 +103,68 @@ doIntegral n = U.sum zs
 
 #if MIN_VERSION_semirings(0,4,2)
 
+gen1 :: Num a => Int -> a
+gen1 k = fromIntegral (truncate (pi * fromIntegral k :: Double) `mod` (k + 1))
+
+gen2 :: Num a => Int -> a
+gen2 k = fromIntegral (truncate (exp 1.0 * fromIntegral k :: Double) `mod` (k + 1))
+
 doQuotRem :: Int -> Double
 doQuotRem n = U.sum (unPoly qs) + U.sum (unPoly rs)
   where
-    xs = toPoly $ U.generate (2 * n) ((+ 1.0) . (* 2.0) . fromIntegral)
-    ys = toPoly $ U.generate n       ((+ 2.0) . (* 3.0) . fromIntegral)
+    xs = toPoly $ U.generate (2 * n) gen1
+    ys = toPoly $ U.generate n       gen2
     (qs, rs) = xs `quotRem` ys
 
 doGcd :: Int -> Integer
 doGcd n = V.sum gs
   where
-    xs = toPoly $ V.generate n ((+ 1) . (* 2) . fromIntegral)
-    ys = toPoly $ V.generate n ((+ 2) . (* 3) . fromIntegral)
+    xs = toPoly $ V.generate n gen1
+    ys = toPoly $ V.generate n gen2
     gs = unPoly $ xs `gcd` ys
 
-doGcdExt :: Int -> Rational
+doGcdExt :: (Eq a, Num a, Field a) => Int -> a
 doGcdExt n = V.sum gs
   where
-    xs = toPoly $ V.generate n ((+ 1) . (* 2) . fromIntegral)
-    ys = toPoly $ V.generate n ((+ 2) . (* 3) . fromIntegral)
+    xs = toPoly $ V.generate n gen1
+    ys = toPoly $ V.generate n gen2
     gs = unPoly $ fst $ xs `gcdExt` ys
 
-doGcdFrac :: Int -> Rational
+doGcdFrac :: (Eq a, Num a, Field a) => Int -> a
 doGcdFrac n = V.sum gs
   where
-    xs = PolyOverField $ toPoly $ V.generate n ((+ 1) . (* 2) . fromIntegral)
-    ys = PolyOverField $ toPoly $ V.generate n ((+ 2) . (* 3) . fromIntegral)
+    xs = PolyOverField $ toPoly $ V.generate n gen1
+    ys = PolyOverField $ toPoly $ V.generate n gen2
     gs = unPoly $ unPolyOverField $ xs `gcd` ys
+
+-- | Inspired by 'semirings'.
+newtype Mod2 = Mod2 { _getMod2 :: Bool }
+  deriving (Eq, NFData)
+
+instance Num Mod2 where
+  (+) = coerce (xor @Bool)
+  (*) = coerce (&&)
+  negate = id
+  abs    = id
+  signum = id
+  fromInteger = Mod2 . odd
+
+instance Semiring Mod2 where
+  plus  = coerce (xor @Bool)
+  times = coerce (&&)
+  fromNatural = Mod2 . odd
+
+instance Ring Mod2 where
+  negate = id
+
+instance GcdDomain Mod2 where
+
+instance Euclidean Mod2 where
+  degree = const 0
+  quotRem x y
+    | isZero y  = throw DivideByZero
+    | otherwise = (x, zero)
+
+instance Field Mod2
 
 #endif
