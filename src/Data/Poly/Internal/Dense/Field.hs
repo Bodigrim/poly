626a28d465afa906e6b350567eb45bcb0b178509
Bodigrim <andrew.lelechenko@gmail.com>
2020-04-06 18:29:16 +0100

    Speed up division for polynomials over fields

--- src/Data/Poly/Internal/Dense/Field.hs
+++ src/Data/Poly/Internal/Dense/Field.hs
@@ -19,13 +19,14 @@ module Data.Poly.Internal.Dense.Field
   ( fieldGcd
   ) where
 
-import Prelude hiding (quotRem, quot, rem, gcd)
+import Prelude hiding (quotRem, quot, rem, gcd, recip)
 import Control.Exception
 import Control.Monad
 import Control.Monad.Primitive
 import Control.Monad.ST
 import Data.Euclidean (Euclidean(..), Field)
-import Data.Semiring (times, minus, zero)
+import Data.Field (recip)
+import Data.Semiring (times, minus, zero, one)
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Generic.Mutable as MG
 
@@ -44,32 +45,40 @@ instance (Eq a, Eq (v a), Field a, G.Vector v a) => Euclidean (Poly v a) where
   {-# INLINE rem #-}
 
 quotientAndRemainder
-  :: (Field a, G.Vector v a)
+  :: (Eq a, Field a, G.Vector v a)
   => v a
   -> v a
   -> (v a, v a)
 quotientAndRemainder xs ys
-  | G.null ys = throw DivideByZero
-  | G.length xs < G.length ys = (G.empty, xs)
+  | lenXs < lenYs = (G.empty, xs)
+  | lenYs == 0 = throw DivideByZero
+  | lenYs == 1 = let invY = recip (G.unsafeHead ys) in
+                 (G.map (`times` invY) xs, G.empty)
   | otherwise = runST $ do
-    let lenXs = G.length xs
-        lenYs = G.length ys
-        lenQs = lenXs - lenYs + 1
     qs <- MG.unsafeNew lenQs
     rs <- MG.unsafeNew lenXs
     G.unsafeCopy rs xs
+    let yLast = G.unsafeLast ys
+        invYLast = recip yLast
     forM_ [lenQs - 1, lenQs - 2 .. 0] $ \i -> do
       r <- MG.unsafeRead rs (lenYs - 1 + i)
-      let q = r `quot` G.unsafeLast ys
+      let q = if yLast == one then r else r `times` invYLast
       MG.unsafeWrite qs i q
-      forM_ [0 .. lenYs - 1] $ \k -> do
-        MG.unsafeModify rs (\c -> c `minus` q `times` G.unsafeIndex ys k) (i + k)
+      MG.unsafeWrite rs (lenYs - 1 + i) zero
+      forM_ [0 .. lenYs - 2] $ \k -> do
+        let y = G.unsafeIndex ys k
+        when (y /= zero) $
+          MG.unsafeModify rs (\c -> c `minus` q `times` y) (i + k)
     let rs' = MG.unsafeSlice 0 lenYs rs
     (,) <$> G.unsafeFreeze qs <*> G.unsafeFreeze rs'
+  where
+    lenXs = G.length xs
+    lenYs = G.length ys
+    lenQs = lenXs - lenYs + 1
 {-# INLINABLE quotientAndRemainder #-}
 
 remainder
-  :: (Field a, G.Vector v a)
+  :: (Eq a, Field a, G.Vector v a)
   => v a
   -> v a
   -> v a
@@ -83,25 +92,29 @@ remainder xs ys
 {-# INLINABLE remainder #-}
 
 remainderM
-  :: (PrimMonad m, Field a, G.Vector v a)
+  :: (PrimMonad m, Eq a, Field a, G.Vector v a)
   => G.Mutable v (PrimState m) a
   -> G.Mutable v (PrimState m) a
   -> m ()
 remainderM xs ys
-  | MG.null ys = throw DivideByZero
-  | MG.length xs < MG.length ys = pure ()
+  | lenXs < lenYs = pure ()
+  | lenYs == 0 = throw DivideByZero
+  | lenYs == 1 = MG.set xs zero
   | otherwise = do
-    let lenXs = MG.length xs
-        lenYs = MG.length ys
-        lenQs = lenXs - lenYs + 1
     yLast <- MG.unsafeRead ys (lenYs - 1)
+    let invYLast = recip yLast
     forM_ [lenQs - 1, lenQs - 2 .. 0] $ \i -> do
       r <- MG.unsafeRead xs (lenYs - 1 + i)
-      forM_ [0 .. lenYs - 1] $ \k -> do
+      MG.unsafeWrite xs (lenYs - 1 + i) zero
+      let q = if yLast == one then r else r `times` invYLast
+      forM_ [0 .. lenYs - 2] $ \k -> do
         y <- MG.unsafeRead ys k
-        -- do not move r / yLast outside the loop,
-        -- because of numerical instability
-        MG.unsafeModify xs (\c -> c `minus` r `times` y `quot` yLast) (i + k)
+        when (y /= zero) $
+          MG.unsafeModify xs (\c -> c `minus` q `times` y) (i + k)
+  where
+    lenXs = MG.length xs
+    lenYs = MG.length ys
+    lenQs = lenXs - lenYs + 1
 {-# INLINABLE remainderM #-}
 
 fieldGcd
